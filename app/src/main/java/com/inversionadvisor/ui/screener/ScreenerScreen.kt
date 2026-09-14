67ad4395985ec30682d733447df05b5b802eb15c
santiatienza-collab <santiatienza@gmail.com>
2026-09-14 19:20:15 +0100

    SUBIR 25

--- app/src/main/java/com/inversionadvisor/ui/screener/ScreenerScreen.kt
+++ app/src/main/java/com/inversionadvisor/ui/screener/ScreenerScreen.kt
@@ -33,7 +33,6 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.ExposedDropdownMenu
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
